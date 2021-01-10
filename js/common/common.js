bbfcb806e0f7cf9b3959dc45f77159be5deb91aa
benwoo1110 <30431861+benwoo1110@users.noreply.github.com>
2021-01-10 13:29:57 +0800

    Use jQuery to load nav bar.

---
+++ js/common/common.js
@@ -0,0 +1,18 @@
+﻿let head = document.getElementById("head");
+
+// Add header styles
+let headerCSS = document.createElement("link");
+headerCSS.setAttribute("rel", "stylesheet");
+headerCSS.setAttribute("href", "css/header-styles.css");
+head.appendChild(headerCSS);
+
+console.log(head)
+
+function doSetup() {
+    $(document).ready(function () {
+        $("#header").load("header.html #nav");
+        console.log($("#header div.navbar-main"));
+    });
+}
+
+doSetup();
