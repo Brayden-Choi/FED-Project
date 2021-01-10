a9c3580a529d7cf514f425dea11433aa9cd1b9e0
benwoo1110 <30431861+benwoo1110@users.noreply.github.com>
2021-01-10 14:36:50 +0800

    Add jQuery to set active nav link.

--- js/common/common.js
+++ js/common/common.js
@@ -1,18 +1,28 @@
-﻿let head = document.getElementById("head");
+﻿// Add header styles
+let head = document.getElementById("head");
 
-// Add header styles
 let headerCSS = document.createElement("link");
 headerCSS.setAttribute("rel", "stylesheet");
 headerCSS.setAttribute("href", "css/header-styles.css");
 head.appendChild(headerCSS);
 
-console.log(head)
+// Add header to html
+$(document).ready(function() {
+    $("#header").load("header.html #nav", setActivePage);
+});
 
-function doSetup() {
-    $(document).ready(function () {
-        $("#header").load("header.html #nav");
-        console.log($("#header div.navbar-main"));
-    });
+function setActivePage() {
+    let targetPage = $("#header").attr("content").toLowerCase();
+    let pages = $("#navbarSupportedContent ul li");
+
+    console.log(pages)
+    console.log(pages.length)
+    
+    for (let page of pages) {
+        let pageName = $(page).find('a:first')[0].innerHTML.toLowerCase();
+        if (pageName === targetPage) {
+            $(page).addClass("active")
+            break;
+        }
+    }
 }
-
-doSetup();
