582b809665e3e75159efe26ba8e3c86cb3d3baaa
benwoo1110 <30431861+benwoo1110@users.noreply.github.com>
2021-01-11 22:51:41 +0800

    First home screen draft.

---
+++ js/home.js
@@ -0,0 +1,27 @@
+﻿$(document).ready(function () {
+
+    let currentShown = 1;
+
+    $('#overviewSlides').on('slide.bs.carousel', function () {
+        let $top = $("#title-" + mod(currentShown - 1, 3));
+        let $current = $("#title-" + currentShown);
+        let $bottom = $("#title-" + mod(currentShown + 1, 3));
+        
+        console.log(currentShown, mod(currentShown - 1, 3), currentShown, mod(currentShown + 1, 3));
+
+        $top.addClass("title-hidden");
+        $top.addClass("title-hidden-bottom");
+        $top.removeClass("title-hidden-top");
+
+        $current.addClass("title-hidden-top");
+
+        $bottom.removeClass("title-hidden");
+        $bottom.removeClass("title-hidden-bottom");
+        
+        currentShown = (currentShown + 1) % 3;
+    })
+});
+
+function mod(n, m) {
+    return ((n % m) + m) % m;
+}
