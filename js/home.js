82e964ff11ee7a66d8691598df34d10f131319c3
benwoo1110 <30431861+benwoo1110@users.noreply.github.com>
2021-01-13 00:18:32 +0800

    Fix overview indicator not updating.

--- js/home.js
+++ js/home.js
@@ -1,5 +1,5 @@
 ﻿$(document).ready(function () {
-    let sm = new SlideManager("#overviewSlides", "#title-", "#overview-desc", "#overview-button");
+    let sm = new SlideManager("#overviewSlides", "#title-", "#overview-desc", "#overview-button", "#overview-indicators");
     const filePath = "multimedia/home/overview.json";
     $.getJSON(filePath).then(function (data) {
         sm.setData(data);
@@ -13,11 +13,12 @@
 });
 
 class SlideManager {
-    constructor(slideId, titlePrefix, desc, button) {
+    constructor(slideId, titlePrefix, desc, button, indicators) {
         this.$slide = $(slideId);
         this.titlePrefix = titlePrefix;
         this.$desc = $(desc);
         this.$button = $(button);
+        this.$indicators = $(indicators);
         this.contentData = {};
         this.currentTitle = 1;
     }
@@ -35,7 +36,7 @@ class SlideManager {
         let $bottom = this.getTitle(1);
         
         this.currentTitle = mod(this.currentTitle + 1, 3);
-        this.setOverviewTexts(event.relatedTarget);
+        this.setOverviewTexts($(event.relatedTarget));
     
         $top.addClass("title-hidden");
         $top.addClass("title-hidden-bottom");
@@ -47,18 +48,28 @@ class SlideManager {
         $bottom.removeClass("title-hidden-bottom");
     }
     
-    setOverviewTexts(targetItem) {
-        if (targetItem === undefined) {
-            targetItem = this.$slide.find(".active")[0];
+    setOverviewTexts($targetItem) {
+        if ($targetItem === undefined) {
+            $targetItem = $(this.$slide.find(".active")[0]);
         }
         
-        const targetData = $(targetItem).attr("content");
+        const targetData = $targetItem.attr("content");
         
         let $title = this.getTitle(0);
         $title.text(targetData);
 
         this.$desc.text(this.contentData[targetData].desc);
         this.$button.attr("href", this.contentData[targetData].page);
+        
+        let activeIndex = $targetItem.index();
+        let index = 0;
+        for (let indicator of this.$indicators.find("li")) {
+            if (index++ === activeIndex) {
+                $(indicator).addClass("active");
+                continue;
+            }
+            $(indicator).removeClass("active");
+        }
     }
     
     getTitle(offset) {
