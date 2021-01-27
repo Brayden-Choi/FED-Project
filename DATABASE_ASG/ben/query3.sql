a6bd0898334b91ae2c97e906029a5d3821a7a5da
benwoo1110 <30431861+benwoo1110@users.noreply.github.com>
2021-01-27 20:58:08 +0800

    Complete 3rd query

--- DATABASE_ASG/ben/query3.sql
+++ DATABASE_ASG/ben/query3.sql
@@ -1,4 +1,20 @@
 /*
-List the food items from Ahmad Makan’s Dinner Menu that was sold at least 3 amount.
+List the food itemID and quantity sold from Ahmad Makan’s Dinner Menu that was sold at least 3 times.
 */
 
+SELECT oi.itemID, SUM(oi.orderQty) 'Quantity Sold'
+
+FROM OrderItem AS oi
+    JOIN MenuItem AS mi
+    ON oi.itemID = mi.itemID
+
+    JOIN Menu as m
+    ON m.outletID = mi.outletID AND m.menuNo = mi.menuNo
+
+    JOIN Outlet as o
+    ON o.outletID = mi.outletID AND m.menuNo = mi.menuNo
+
+WHERE (o.outletName = 'Ahmad Makan' AND m.menuName = 'Dinner')
+
+GROUP BY oi.itemID
+HAVING SUM(oi.orderQty) >= 3
