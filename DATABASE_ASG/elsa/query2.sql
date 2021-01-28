f37a13c5c94c078e77b75440d3f7c5d07cf62358
Elsa Lee <s10205942@connect.np.edu.sg>
2021-01-29 01:04:16 +0800

    edited query

--- DATABASE_ASG/elsa/query2.sql
+++ DATABASE_ASG/elsa/query2.sql
@@ -1,4 +1,4 @@
-﻿-- List all the riders that use a Car or an E-Scooter and delivered orders that were more than $5 and had a 10% promotions.
+﻿-- List all the riders that use a Motorcycle or an E-Scooter and delivered orders that were more than $5 and had a 10% promotions.
 SELECT r.* FROM Rider r
 INNER JOIN Delivery d
 ON r.riderID = d.riderID
@@ -8,7 +8,7 @@ INNER JOIN payment pa
 ON op.orderID = pa.orderID
 INNER JOIN Promotion p
 ON op.promoID = p.promoID
-WHERE (r.deliveryMode = 'Car' 
+WHERE (r.deliveryMode = 'Motorcycle' 
 OR r.deliveryMode = 'E-Scooter')
 AND pa.pmtAmt > 5
 AND p.percentDiscount = 10
