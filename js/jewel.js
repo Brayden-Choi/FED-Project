89516487acd0b22218eeeddb591660d824110fb8
BraydenChoi <s10205405@connect.np.edu.sg>
2021-01-31 22:21:47 +0800

    doing my stupid form

--- js/jewel.js
+++ js/jewel.js
@@ -2,7 +2,7 @@
 $(document).ready(function () {
     $("#contactform").on("submit", function (event) {
         event.preventDefault();
-        alert("Thank you for your submission, our dedicated staff will get back to you soon!");
+        alert("Thank you for your feedback/questions, we will get back to you within 5 working days!");
         $("#form").trigger("reset");
     });
 });
