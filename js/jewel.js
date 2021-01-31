20b97e255573a6764cd3334c5f40efbd81c287af
BraydenChoi <s10205405@connect.np.edu.sg>
2021-01-31 21:10:58 +0800

    adding form js

---
+++ js/jewel.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function () {
+    $("#contact-form").on('submit', function (event) {
+        event.preventDefault();
+        var name = document.getElementById("contact_nom").value;
+        document.getElementById("output").innerHTML = "Thank you " + name + ". We will get back to you within 5 days!"
+    });
+
+    $("#contact-form").on('reset', function (event) {
+        document.getElementById("p").innerHTML = "";
+    });
+})
