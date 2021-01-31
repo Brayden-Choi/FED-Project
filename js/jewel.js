9f1e072a2bdae4fe71351159202a3da3dc782da4
BraydenChoi <s10205405@connect.np.edu.sg>
2021-02-01 00:59:18 +0800

    gg

--- js/jewel.js
+++ js/jewel.js
@@ -3,11 +3,6 @@ $(document).ready(function () {
     $("#contactform").on("submit", function (event) {
         event.preventDefault();
         alert("Thank you for your feedback/questions, we will get back to you within 5 working days!");
-        $("#form").trigger("reset");
-    });
-});
-
-    $("#contactform").on('reset', function (event) {
-        document.getElementById("p").innerHTML = "";
+        $("#contactform").trigger("reset");
     });
 })
